b03661fb9a9ff650d156d89921b56b77003d8485
Tomáš Diblík <dibla.tomas@post.cz>
2023-07-20 17:00:45 +0200

    [Wip] Sidebar item

--- src/routes/app/Sidebar.svelte
+++ src/routes/app/Sidebar.svelte
@@ -1,10 +1,16 @@
 <script>
+  import CalendarIcon from "../../utils/CalendarIcon.svelte";
+  import HouseIcon from "../../utils/HouseIcon.svelte";
+  import ListIcon from "../../utils/ListIcon.svelte";
+  import PlusIcon from "../../utils/PlusIcon.svelte";
+  import SunIcon from "../../utils/SunIcon.svelte";
+  import SidebarItem from "./SidebarItem.svelte";
   import {current_user_account} from "./user_account_store";
 
   const base_li_classes = "ml-2 mr-2";
 </script>
 
-<ul class="menu p-4 w-70 h-full bg-base-200 text-base-content">
+<ul class="menu p-4 w-70 min-h-full bg-base-200 text-base-content">
   <li>
     <button class="h-14 mb-2 mr-auto ml-auto w-full md:w-auto flex">
       <img class="h-12 rounded-full" src={$current_user_account.profile_photo} alt="Account's profile pic" />
@@ -18,10 +24,31 @@
     <input type="text" placeholder="Search" class="input input-bordered input-sm" />
   </li>
 
+  <ul class="menu bg-base-200 min-w-56 rounded-box pb-0">
+    <SidebarItem icon={SunIcon} title="My Day" number_of_tasks={1} on_click={() => {}} />
+    <SidebarItem icon={CalendarIcon} title="Planned" number_of_tasks={2} on_click={() => {}} />
+    <SidebarItem icon={HouseIcon} title="Tasks" number_of_tasks={3} on_click={() => {}} />
+  </ul>
+
   <!-- Acts as a divider. More details at daisyui docs. -->
   <li />
 
-  <li class={base_li_classes}>
-    <p>lajse</p>
-  </li>
+  <!--TODO: Implement groups, microsoft graph api does not return them atm, so either make my own implementaiton or wait for microsfot to implement it ----- Example groups using DaisyUI: https://daisyui.com/components/menu/#collapsible-submenu -->
+  <ul class="menu bg-base-200 min-w-56 rounded-box pt-0 pb-0">
+    <SidebarItem icon={ListIcon} title="AAAA" number_of_tasks={4} on_click={() => {}} />
+    <SidebarItem icon={ListIcon} title="Things to listen to in free time" number_of_tasks={9} on_click={() => {}} />
+  </ul>
+
+  <div class="mt-auto">
+    <!-- Acts as a divider. More details at daisyui docs. -->
+    <li />
+
+    <li class={base_li_classes}>
+      <button class="pl-2">
+        <PlusIcon />
+        <span class="ml-2">New list</span>
+        <!-- TODO: "Add groups icon" after implementing groups -->
+      </button>
+    </li>
+  </div>
 </ul>
---
+++ src/routes/app/SidebarItem.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type {ComponentType} from "svelte";
+
+  export let icon: ComponentType;
+  export let title: string;
+  export let number_of_tasks: number;
+  export let on_click: () => void;
+
+  const MAX_TITLE_LENGTH = 30;
+  if (title.length > MAX_TITLE_LENGTH) {
+    title = title.substring(0, MAX_TITLE_LENGTH) + "...";
+  }
+</script>
+
+<button on:click={() => on_click()}>
+  <li>
+    <span class="pl-1">
+      <span class="w-6">
+        <svelte:component this={icon} />
+      </span>
+      {title}
+      {#if number_of_tasks > 0}
+        <span class=" rounded-full bg-slate-900 pr-1 pl-1"> {number_of_tasks} </span>
+      {/if}
+    </span>
+  </li>
+</button>
